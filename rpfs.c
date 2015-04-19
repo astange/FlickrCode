f8b5e79ea189dfb4021663247fc5a33cea81f09b
Anthony <astange@Anthonys-MBP.hsd1.ga.comcast.net>
2015-04-19 18:24:42 -0400

    Starting from scratch.

--- rpfs.c
+++ rpfs.c
@@ -6,9 +6,75 @@
 #define FUSE_USE_VERSION 30
 #include <fuse.h>
 #include <sys/time.h>
+#include <fcntl.h>
 #include "uthash.h"
 #include "python_caller.h"
 
+static const char *hello_str = "Hello World!\n";
+static const char *hello_path = "/hello";
+static int hello_getattr(const char *path, struct stat *stbuf)
+{
+        int res = 0;
+        memset(stbuf, 0, sizeof(struct stat));
+        if (strcmp(path, "/") == 0) {
+                stbuf->st_mode = S_IFDIR | 0755;
+                stbuf->st_nlink = 2;
+        } else if (strcmp(path, hello_path) == 0) {
+                stbuf->st_mode = S_IFREG | 0444;
+                stbuf->st_nlink = 1;
+                stbuf->st_size = strlen(hello_str);
+        } else
+                res = -ENOENT;
+        return res;
+}
+static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                         off_t offset, struct fuse_file_info *fi)
+{
+        (void) offset;
+        (void) fi;
+        if (strcmp(path, "/") != 0)
+                return -ENOENT;
+        filler(buf, ".", NULL, 0);
+        filler(buf, "..", NULL, 0);
+        filler(buf, hello_path + 1, NULL, 0);
+        return 0;
+}
+static int hello_open(const char *path, struct fuse_file_info *fi)
+{
+        if (strcmp(path, hello_path) != 0)
+                return -ENOENT;
+        if ((fi->flags & 3) != O_RDONLY)
+                return -EACCES;
+        return 0;
+}
+static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+                      struct fuse_file_info *fi)
+{
+        size_t len;
+        (void) fi;
+        if(strcmp(path, hello_path) != 0)
+                return -ENOENT;
+        len = strlen(hello_str);
+        if (offset < len) {
+                if (offset + size > len)
+                        size = len - offset;
+                memcpy(buf, hello_str + offset, size);
+        } else
+                size = 0;
+        return size;
+}
+static struct fuse_operations hello_oper = {
+        .getattr        = hello_getattr,
+        .readdir        = hello_readdir,
+        .open           = hello_open,
+        .read           = hello_read,
+};
+int main(int argc, char *argv[])
+{
+        return fuse_main(argc, argv, &hello_oper, NULL);
+}
+
+/*
 struct photo {
     char md5string[MD5_DIGEST_LENGTH*2+1];
     unsigned long long id;
@@ -27,7 +93,7 @@ static int rpfs_getattr(const char *path, struct stat *stbuf)
         stbuf->st_nlink = 1;
         stbuf->st_uid = getuid();
         stbuf->st_gid = getgid();
-        stbuf->st_size = (1ULL << 32); /* 4G */
+        stbuf->st_size = (1ULL << 32); // 4G 
         stbuf->st_blocks = 0;
         stbuf->st_atime = stbuf->st_mtime = stbuf->st_ctime = time(NULL);
         return 0;
@@ -125,4 +191,4 @@ struct fuse_operations rpfs_oper = {
 
 int main(int argc, char *argv[]) {
     return fuse_main(argc, argv, &rpfs_oper, NULL);
-}
+}*/
