ad722c6674d75ccebf519ea93d223e6a68a7cb1f
Anthony <astange@ipsec-143-215-27-93.vpn.gatech.edu>
2015-04-19 18:50:33 -0400

    asdfas

--- rpfs.c
+++ rpfs.c
@@ -11,16 +11,16 @@
 #include "python_caller.h"
 
 static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+static const char *master_path = "/master.node";
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
         int res = 0;
         memset(stbuf, 0, sizeof(struct stat));
         if (strcmp(path, "/") == 0) {
-                stbuf->st_mode = S_IFDIR | 0755;
+                stbuf->st_mode = S_IFDIR | 0777;
                 stbuf->st_nlink = 2;
-        } else if (strcmp(path, hello_path) == 0) {
-                stbuf->st_mode = S_IFREG | 0444;
+        } else if (strcmp(path, master_path) == 0) {
+                stbuf->st_mode = S_IFREG | 0777;
                 stbuf->st_nlink = 1;
                 stbuf->st_size = strlen(hello_str);
         } else
@@ -36,15 +36,15 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                 return -ENOENT;
         filler(buf, ".", NULL, 0);
         filler(buf, "..", NULL, 0);
-        filler(buf, hello_path + 1, NULL, 0);
+        filler(buf, master_path + 1, NULL, 0);
         return 0;
 }
 static int hello_open(const char *path, struct fuse_file_info *fi)
 {
-        if (strcmp(path, hello_path) != 0)
+        /*if (strcmp(path, master_path) != 0)
                 return -ENOENT;
         if ((fi->flags & 3) != O_RDONLY)
-                return -EACCES;
+                return -EACCES;*/
         return 0;
 }
 static int hello_read(const char *path, char *buf, size_t size, off_t offset,
@@ -52,7 +52,7 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 {
         size_t len;
         (void) fi;
-        if(strcmp(path, hello_path) != 0)
+        if(strcmp(path, master_path) != 0)
                 return -ENOENT;
         len = strlen(hello_str);
         if (offset < len) {
@@ -63,10 +63,85 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
                 size = 0;
         return size;
 }
+
+static int hello_write(const char *path, const char *buf, size_t size, off_t offset,
+    struct fuse_file_info *fi) {
+
+    printf("Hi\n");
+    struct timeval tstartFull;
+    struct timeval tstartFullEnd;
+    gettimeofday(&tstartFull,NULL);
+
+    // Check path validity
+    int i;
+    for (i=0; i<strlen(path); i++) {
+        if (i==0 && path[i]!='/')
+            return -ENOENT;
+        if (i!=0 && path[i]=='/')
+            return -ENOENT;
+    }
+
+    // Extract instruction and filename
+    int index = 0;
+    for (i=0; i<strlen(buf);i++) {
+        if (buf[i]==' ') {
+            index = i;
+            break;
+        }
+    }
+
+    char* instr = malloc(index);
+    char* filename = malloc(strlen(buf)-index-1);
+
+    memcpy(instr, buf, index);
+    memcpy(filename, &buf[index+2], strlen(buf)-index);
+
+    // Check instruction validity
+    if (strcmp(instr, "get")!=0 && strcmp(instr, "post")!=0)
+        return -1;
+
+    // Check filename path validity, maybe not needed
+    for (i=0; i<strlen(filename); i++)
+        if (filename[i]=='/')
+            return -ENOENT;
+
+
+    // Hash
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    MD5_CTX context;
+    MD5_Init(&context);
+    MD5_Update(&context, filename, strlen(filename));
+    MD5_Final(digest, &context);
+
+    // MD5 to String
+    char md5string[MD5_DIGEST_LENGTH*2+1];
+    for(i = 0; i < MD5_DIGEST_LENGTH; ++i)
+        sprintf(&md5string[i*2], "%02x", (unsigned int)digest[i]);
+
+    struct photo* p;
+    // get - check hash - python get
+    if (strcmp(instr, "get")==0) {
+        HASH_FIND_STR(photos, md5string, p);
+        get(p->id);
+    }
+
+    // post - hash - if file exist, md5 hash, python
+    if (strcmp(instr, "post")==0) {
+        p = (struct photo*) malloc(sizeof(struct photo));
+        memcpy(p->md5string, md5string, strlen(md5string));
+        //p->md5string = md5string;
+        p->id = put(filename);
+        HASH_ADD_STR(photos, md5string, p);
+    }
+    gettimeofday(&tstartFullEnd, NULL);
+    printf("Time spent performing entire operation: %4ld seconds and %d microseconds\n", tstartFullEnd.tv_sec - tstartFull.tv_sec, tstartFullEnd.tv_usec - tstartFull.tv_usec);
+    return size;
+}
 static struct fuse_operations hello_oper = {
         .getattr        = hello_getattr,
         .readdir        = hello_readdir,
         .open           = hello_open,
+        .write          = hello_write,
         .read           = hello_read,
 };
 int main(int argc, char *argv[])
