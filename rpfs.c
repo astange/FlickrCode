8d6bb40da1290297d4c18799ac1b98e05e9e6452
Anthony <astange@ipsec-143-215-27-100.vpn.gatech.edu>
2015-04-20 02:56:56 -0400

    asdfa

--- rpfs.c
+++ rpfs.c
@@ -264,11 +264,16 @@ static int rpfs_write(const char *path, const char *buf, size_t size, off_t offs
     }
 
     char* instr = malloc(index);
-    char* filename = malloc(strlen(buf)-index-1);
+    char* filename = malloc(strlen(buf)-index);
 
     memcpy(instr, buf, index);
     memcpy(filename, &buf[index+1], strlen(buf)-index);
-
+    i = 0;
+    while(filename[i] != ' ')
+    {
+        i++;
+    }
+    filename[i] = '\0';
     printf("%s\n", instr);
     printf("%s\n", filename);
 
