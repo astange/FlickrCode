1848853908baf271f7228e5a8bb275ed5342314f
Michael Dunn <mdunn31@gmail.com>
2015-04-20 00:40:26 -0400

    added some malloc stuff maybe?

--- rpfs.c
+++ rpfs.c
@@ -268,11 +268,13 @@ static struct fuse_operations rpfs_oper = {
 static int rpfs_init(int backups,int argc, char *argv[]){
     backupNum=backups;
     int i = 0;
+    *nodeListing = (char*)malloc(sizeof(char)*100);
+    nodeListing = (char**)malloc(sizeof(char*)*100);
     char *intToChar = NULL;
     for(i; i<backups; i++){
         sprintf(intToChar,"%d", i);
-        //strcpy(nodeListing[i], "/BackupNode_");
-        //strcat(nodeListing[i], intToChar);
+        strcpy(nodeListing[i], "/BackupNode_");
+        strcat(nodeListing[i], intToChar);
     }
     i = 0;
     for (i; i < backups; i++) {
